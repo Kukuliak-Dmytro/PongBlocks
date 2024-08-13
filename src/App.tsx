7241c5d8f9ce28cdfe0609541af126d1cdf5830f
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-08-13 16:16:33 +0300

    The ball position shall be stored in the main file for convenience

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import React, { useRef, useEffect } from 'react';
+import  { useRef, useEffect } from 'react';
 import './App.css';
-import Ball, { BallHandle } from './Ball';
+import Ball from './Ball';
 import Paddle from './Paddle';
 import Block from './Block';
-
+function randomNumberBetween(min:number, max:number) {
+  return Math.random() * (max - min) + min;
+}
 function App() {
-    const ballRef = useRef<BallHandle>(null);
+  const INITIAL_VELOCITY = 0.025;
+  const VELOCITY_INCREASE = .000001;
     let lastTime: number;
-
+    let ballPosition={x:50,y:50}
+    let direction={x:0,y:0}
+    let velocity=0;
+    function resetBall(){
+      ballPosition.x = 50;
+      ballPosition.y = 50;
+      direction = { x: 0, y: 0 };
+      velocity = INITIAL_VELOCITY;
+      while (
+          Math.abs(direction.x) <= .2 ||
+          Math.abs(direction.x) >= .9
+      ) {
+          const heading = randomNumberBetween(0, 2 * Math.PI);
+          direction = { x: Math.cos(heading), y: Math.sin(heading) };
+      }
+    }
+    function updateBall(delta:number){
+      ballPosition.x += direction.x * velocity * delta;
+      ballPosition.y += direction.y * velocity * delta;
+      velocity += VELOCITY_INCREASE * delta;
+  }
     function update(time: number) {
         if (lastTime != null) {
             const delta = time - lastTime;
-            ballRef.current?.BallUpdate(delta);
+            updateBall(delta)
         }
         lastTime = time;
         window.requestAnimationFrame(update);
@@ -24,10 +47,19 @@ function App() {
     return (
         <>
             <div className="blockWrapper">
-                {/* Your blocks here */}
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
+               <Block></Block>
             </div>
             <Paddle />
-            <Ball ref={ballRef} />
+            <Ball position={ballPosition} />
         </>
     );
 }
